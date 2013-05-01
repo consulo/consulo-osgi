ce259349f6e553f0dd0dc3325ab989b323b612e9
VISTALL <vistall.valeriy@gmail.com>
2013-05-01 22:16:35 +0300

    typo

--- src/org/jetbrains/osgi/OSGiIcons.java
+++ src/org/jetbrains/osgi/OSGiIcons.java
@@ -9,10 +9,10 @@ import javax.swing.*;
  * @since 17:25/27.04.13
  */
 public interface OSGiIcons {
-  Icon FacetType = IconLoader.findIcon("/org/jetbrains/osmorc2/icons/osgiFacet.png");
-  Icon ExportPackage = IconLoader.findIcon("/org/jetbrains/osmorc2/icons/exportPackage.png");
-  Icon ImportPackage = IconLoader.findIcon("/org/jetbrains/osmorc2/icons/importPackage.png");
-  Icon OsgiComponentFile = IconLoader.findIcon("/org/jetbrains/osmorc2/icons/osgiComponentFile.png");
-  Icon OsgiComponent = IconLoader.findIcon("/org/jetbrains/osmorc2/icons/osgiComponent.png");
-  Icon OsgiBundleActivator = IconLoader.findIcon("/org/jetbrains/osmorc2/icons/osgiBundleActivator.png");
+  Icon FacetType = IconLoader.findIcon("/org/jetbrains/osgi/icons/osgiFacet.png");
+  Icon ExportPackage = IconLoader.findIcon("/org/jetbrains/osgi/icons/exportPackage.png");
+  Icon ImportPackage = IconLoader.findIcon("/org/jetbrains/osgi/icons/importPackage.png");
+  Icon OsgiComponentFile = IconLoader.findIcon("/org/jetbrains/osgi/icons/osgiComponentFile.png");
+  Icon OsgiComponent = IconLoader.findIcon("/org/jetbrains/osgi/icons/osgiComponent.png");
+  Icon OsgiBundleActivator = IconLoader.findIcon("/org/jetbrains/osgi/icons/osgiBundleActivator.png");
 }
