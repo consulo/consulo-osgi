394659f5eb98cb401a7feb6f8934385739d88556
VISTALL <vistall.valeriy@gmail.com>
2013-05-13 19:25:13 +0300

    support linemarker for component providers

--- src/org/jetbrains/osgi/OSGiConstants.java
+++ src/org/jetbrains/osgi/OSGiConstants.java
@@ -1,5 +1,7 @@
 package org.jetbrains.osgi;
 
+import aQute.bnd.annotation.component.Component;
+
 /**
  * @author VISTALL
  * @since 1:12/29.04.13
@@ -8,6 +10,8 @@ public interface OSGiConstants {
   String OSGI_INFO_ROOT = "OSGI-INF";
   String META_INFO_ROOT = "META-INF";
   String MANIFEST_NAME = "MANIFEST.MF";
+
+  String COMPONENT_ANNOTATION = Component.class.getName();
 
   String DEFAULT_OSGI_INF_ROOT_LOCATION = "$MODULE_DIR$/OSGI-INF";
   String DEFAULT_META_INF_ROOT_LOCATION = "$MODULE_DIR$/META-INF";
--- src/org/jetbrains/osgi/ide/OSGiPsiClassLineMarkerFactory.java
+++ src/org/jetbrains/osgi/ide/OSGiPsiClassLineMarkerFactory.java
@@ -1,16 +1,19 @@
 package org.jetbrains.osgi.ide;
 
-import aQute.bnd.annotation.component.Component;
 import com.intellij.codeHighlighting.Pass;
 import com.intellij.codeInsight.AnnotationUtil;
 import com.intellij.codeInsight.daemon.GutterIconNavigationHandler;
 import com.intellij.codeInsight.daemon.LineMarkerInfo;
 import com.intellij.codeInsight.daemon.impl.PsiElementListNavigator;
+import com.intellij.codeInsight.navigation.ClassImplementationsSearch;
 import com.intellij.ide.util.DefaultPsiElementCellRenderer;
 import com.intellij.openapi.editor.markup.GutterIconRenderer;
 import com.intellij.psi.*;
+import com.intellij.psi.util.PsiTypesUtil;
 import com.intellij.util.ConstantFunction;
+import com.intellij.util.Processor;
 import org.jetbrains.annotations.NotNull;
+import org.jetbrains.osgi.OSGiConstants;
 import org.jetbrains.osgi.OSGiIcons;
 import org.jetbrains.osgi.facet.OSGiFacet;
 import org.jetbrains.osgi.facet.OSGiFacetUtil;
@@ -18,6 +21,8 @@ import org.jetbrains.osgi.manifest.BundleManifest;
 import org.osgi.framework.Constants;
 
 import java.awt.event.MouseEvent;
+import java.util.ArrayList;
+import java.util.List;
 
 /**
  * @author VISTALL
@@ -44,8 +49,8 @@ public enum OSGiPsiClassLineMarkerFactory {
 
     @Override
     public LineMarkerInfo<PsiElement> getLineMarker(@NotNull PsiIdentifier nameIdentifier, @NotNull PsiClass psiClass, @NotNull OSGiFacet facet) {
-      return OSGiFacetUtil.isBundleActivator(psiClass) ? new LineMarkerInfo<PsiElement>(nameIdentifier, nameIdentifier.getTextRange(), OSGiIcons.OsgiBundleActivator,
-                                                                                        Pass.UPDATE_OVERRIDEN_MARKERS,
+      return OSGiFacetUtil.isBundleActivator(psiClass) ? new LineMarkerInfo<PsiElement>(nameIdentifier, nameIdentifier.getTextRange(),
+                                                                                        OSGiIcons.OsgiBundleActivator, Pass.UPDATE_OVERRIDEN_MARKERS,
                                                                                         new ConstantFunction<PsiElement, String>("Bundle activator"),
                                                                                         myNavigationHandler, GutterIconRenderer.Alignment.LEFT) : null;
     }
@@ -53,10 +58,10 @@ public enum OSGiPsiClassLineMarkerFactory {
   COMPONENT_IMPLEMENTATION_BY_ANNOTATION {
     @Override
     public LineMarkerInfo<PsiElement> getLineMarker(@NotNull PsiIdentifier nameIdentifier, @NotNull PsiClass psiClass, @NotNull OSGiFacet facet) {
-      PsiAnnotation annotation = AnnotationUtil.findAnnotation(psiClass, Component.class.getName());
+      PsiAnnotation annotation = AnnotationUtil.findAnnotation(psiClass, OSGiConstants.COMPONENT_ANNOTATION);
       if (annotation != null) {
         return new LineMarkerInfo<PsiElement>(nameIdentifier, nameIdentifier.getTextRange(), OSGiIcons.OsgiComponentImplementation,
-                                              Pass.UPDATE_OVERRIDEN_MARKERS, new ConstantFunction<PsiElement, String>("OSGi component"), null,
+                                              Pass.UPDATE_OVERRIDEN_MARKERS, new ConstantFunction<PsiElement, String>("OSGi component implementation"), null,
                                               GutterIconRenderer.Alignment.LEFT);
       }
       return null;
@@ -64,8 +69,70 @@ public enum OSGiPsiClassLineMarkerFactory {
   },
   COMPONENT_INTERFACE_BY_ANNOTATION {
     @Override
-    public LineMarkerInfo<PsiElement> getLineMarker(@NotNull PsiIdentifier nameIdentifier, @NotNull PsiClass psiClass, @NotNull OSGiFacet facet) {
-      return null;
+    public LineMarkerInfo<PsiElement> getLineMarker(@NotNull PsiIdentifier nameIdentifier, @NotNull final PsiClass psiClass, @NotNull OSGiFacet facet) {
+
+      final List<PsiClass> classes = collectImplementations(psiClass);
+      if (classes.isEmpty()) {
+        return null;
+      }
+
+      return new LineMarkerInfo<PsiElement>(nameIdentifier, nameIdentifier.getTextRange(), OSGiIcons.OsgiComponentInterface, Pass.UPDATE_OVERRIDEN_MARKERS,
+                                            new ConstantFunction<PsiElement, String>("OSGi component provider"), new GutterIconNavigationHandler<PsiElement>() {
+        @Override
+        public void navigate(MouseEvent e, PsiElement elt) {
+          final List<PsiClass> classes = collectImplementations(psiClass);
+          if(classes.isEmpty()) {
+            return;
+          }
+          PsiElementListNavigator.openTargets(e, classes.toArray(new NavigatablePsiElement[classes.size()]), "Navigate to implementation", "Navigate to implementation", new DefaultPsiElementCellRenderer());
+        }
+      }, GutterIconRenderer.Alignment.LEFT);
+    }
+
+    private List<PsiClass> collectImplementations(@NotNull final PsiClass owner) {
+      final List<PsiClass> list = new ArrayList<PsiClass>(2);
+      if (CommonClassNames.JAVA_LANG_OBJECT.equals(owner.getQualifiedName())) {
+        return null;
+      }
+
+      ClassImplementationsSearch.processImplementations(owner, new Processor<PsiClass>() {
+        @Override
+        public boolean process(PsiClass iterationClass) {
+          PsiAnnotation annotation = AnnotationUtil.findAnnotation(iterationClass, OSGiConstants.COMPONENT_ANNOTATION);
+          if (annotation == null) {
+            return true;
+          }
+
+          final PsiAnnotationMemberValue provide = annotation.findDeclaredAttributeValue("provide");
+          if (provide == null) {
+            for (PsiClass interfacePsiClass : iterationClass.getInterfaces()) {
+              if (interfacePsiClass.isEquivalentTo(owner)) {
+                list.add(iterationClass);
+              }
+            }
+          }
+          else if (provide instanceof PsiArrayInitializerMemberValue) {
+            for (PsiAnnotationMemberValue initializer : ((PsiArrayInitializerMemberValue)provide).getInitializers()) {
+              if (initializer instanceof PsiClassObjectAccessExpression) {
+                final PsiType type = ((PsiClassObjectAccessExpression)initializer).getOperand().getType();
+                final PsiClass psiClassOfType = PsiTypesUtil.getPsiClass(type);
+                if (owner.isEquivalentTo(psiClassOfType)) {
+                  list.add(iterationClass);
+                }
+              }
+            }
+          }
+          else if (provide instanceof PsiClassObjectAccessExpression) {
+            final PsiType type = ((PsiClassObjectAccessExpression)provide).getOperand().getType();
+            final PsiClass psiClassOfType = PsiTypesUtil.getPsiClass(type);
+            if (owner.isEquivalentTo(psiClassOfType)) {
+              list.add(iterationClass);
+            }
+          }
+          return true;
+        }
+      });
+      return list;
     }
   },
   COMPONENT_IMPLEMENTATION_BY_XML {
