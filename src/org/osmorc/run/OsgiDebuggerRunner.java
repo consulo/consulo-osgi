00c1734499bf3ec10e8cd466d0a4b3490aedd666
VISTALL <vistall.valeriy@gmail.com>
2014-09-07 20:39:28 +0400

    compilation

--- src/org/osmorc/run/OsgiDebuggerRunner.java
+++ src/org/osmorc/run/OsgiDebuggerRunner.java
@@ -1,15 +1,18 @@
 package org.osmorc.run;
 
+import org.jetbrains.annotations.NotNull;
 import com.intellij.debugger.engine.DebuggerUtils;
 import com.intellij.debugger.impl.GenericDebuggerRunner;
 import com.intellij.execution.ExecutionException;
-import com.intellij.execution.configurations.*;
+import com.intellij.execution.configurations.DebuggingRunnerData;
+import com.intellij.execution.configurations.RemoteConnection;
+import com.intellij.execution.configurations.RunProfile;
+import com.intellij.execution.configurations.RunProfileState;
+import com.intellij.execution.configurations.RunnerSettings;
 import com.intellij.execution.executors.DefaultDebugExecutor;
 import com.intellij.execution.runners.ExecutionEnvironment;
 import com.intellij.execution.ui.RunContentDescriptor;
 import com.intellij.openapi.diagnostic.Logger;
-import com.intellij.openapi.project.Project;
-import org.jetbrains.annotations.NotNull;
 
 /**
  * Implementation of {@link GenericDebuggerRunner}
@@ -17,50 +20,57 @@ import org.jetbrains.annotations.NotNull;
  * @author <a href="janthomae@janthomae.de">Jan Thom&auml;</a>
  * @version $Id:$
  */
-public class OsgiDebuggerRunner extends GenericDebuggerRunner {
-  private final Logger logger = Logger.getInstance("#org.osmorc.run.OsgiDebuggerRunner");
+public class OsgiDebuggerRunner extends GenericDebuggerRunner
+{
+	private final Logger logger = Logger.getInstance("#org.osmorc.run.OsgiDebuggerRunner");
 
-  @Override
-  public boolean canRun(@NotNull String executorId, @NotNull RunProfile profile) {
-    return executorId.equals(DefaultDebugExecutor.EXECUTOR_ID) && profile instanceof OsgiRunConfiguration;
-  }
+	@Override
+	public boolean canRun(@NotNull String executorId, @NotNull RunProfile profile)
+	{
+		return executorId.equals(DefaultDebugExecutor.EXECUTOR_ID) && profile instanceof OsgiRunConfiguration;
+	}
 
-  @NotNull
-  @Override
-  public String getRunnerId() {
-    return "OsgiDebugRunner";
-  }
+	@NotNull
+	@Override
+	public String getRunnerId()
+	{
+		return "OsgiDebugRunner";
+	}
 
-  @Override
-  protected RunContentDescriptor createContentDescriptor(Project project,
-                                                         RunProfileState state,
-                                                         RunContentDescriptor contentToReuse,
-                                                         ExecutionEnvironment env) throws ExecutionException {
-    OsgiRunState runState = (OsgiRunState)state;
-    final RunnerSettings myRunnerSettings = ((OsgiRunState) state).getRunnerSettings();
+	@Override
+	protected RunContentDescriptor createContentDescriptor(@NotNull RunProfileState state, @NotNull ExecutionEnvironment env) throws ExecutionException
+	{
+		OsgiRunState runState = (OsgiRunState) state;
+		final RunnerSettings myRunnerSettings = ((OsgiRunState) state).getRunnerSettings();
 
-    if (runState.requiresRemoteDebugger()) {
-      // this is actually copied from the default, but well
-      String myDebugPort = null;
-      if (myRunnerSettings instanceof DebuggingRunnerData) {
-        myDebugPort = ((DebuggingRunnerData)myRunnerSettings).getDebugPort();
-        if (myDebugPort.length() == 0) {
-          try {
-            myDebugPort = DebuggerUtils.getInstance().findAvailableDebugAddress(true);
-          }
-          catch (ExecutionException e) {
-            logger.error(e);
-          }
-          ((DebuggingRunnerData)myRunnerSettings).setDebugPort(myDebugPort);
-        }
-        ((DebuggingRunnerData)myRunnerSettings).setLocal(false);
-      }
-      final RemoteConnection connection = new RemoteConnection(true, "127.0.0.1", myDebugPort, true);
-      return attachVirtualMachine(project, state, contentToReuse, env, connection, false);
-    }
-    else {
-      // let the default debugger do it's job.
-      return super.createContentDescriptor(project, state, contentToReuse, env);
-    }
-  }
+		if(runState.requiresRemoteDebugger())
+		{
+			// this is actually copied from the default, but well
+			String myDebugPort = null;
+			if(myRunnerSettings instanceof DebuggingRunnerData)
+			{
+				myDebugPort = ((DebuggingRunnerData) myRunnerSettings).getDebugPort();
+				if(myDebugPort.length() == 0)
+				{
+					try
+					{
+						myDebugPort = DebuggerUtils.getInstance().findAvailableDebugAddress(true);
+					}
+					catch(ExecutionException e)
+					{
+						logger.error(e);
+					}
+					((DebuggingRunnerData) myRunnerSettings).setDebugPort(myDebugPort);
+				}
+				((DebuggingRunnerData) myRunnerSettings).setLocal(false);
+			}
+			final RemoteConnection connection = new RemoteConnection(true, "127.0.0.1", myDebugPort, true);
+			return attachVirtualMachine(state, env, connection, false);
+		}
+		else
+		{
+			// let the default debugger do it's job.
+			return super.createContentDescriptor(state, env);
+		}
+	}
 }
